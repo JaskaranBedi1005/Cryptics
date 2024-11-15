9afb1822755d7e41a9b19d9aebe5968e3dcb6b2c
Hrideya <hrideyasharma7@gmail.com>
2024-11-15 22:15:44 +0530

    progress

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def funct():
+def calories():
+    stapleFood = ['Roti','Naan','Paratha','Puri','Idli','Dosa','Uttapam','Upma','Khichdi','Biryani']
+    veg = ['pBM','palakPaneer','alooGobi','Bharta','Chole','Rajma','Bhindi','malaiKofta','Sambar','Dosa']
+    nonVeg = ['butterChicken','cTM','fishCurry','mRJ','Keema','tandooriChicken','Prawn','eggCurry','chickenBiryani','Korma']
+    snacks = ['Samosa','Pakora','bhelPuri','paniPuri','vadaPav','Dhokla','alooTikki','pavBhaji','Kachori','Chaat','Chips']
+    sweets = ['gulabJamun','Rasgulla','Jalebi','Laddu','Kheer','Barfi','Rasmalai','Halwa','Payasam','mysorePak']
     categories = [stapleFood,veg,nonVeg,snacks,sweets]
-    stapleFood = ['Roti']
-    veg = []
-    nonVeg = []
-    snacks = []
-    sweets = []
+    print(categories[0][3])
+calories()

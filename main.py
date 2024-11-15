1ea4e6580adf846cb903291260d38be03d00533e
Hrideya <hrideyasharma7@gmail.com>
2024-11-16 02:31:47 +0530

    Added linking functionality

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask
-def calories():
-    stapleFood = {'Roti': 120,'Naan': 270,'Paratha': 500,'Puri': 130,'Idli': 70,'Dosa':350,'Uttapam':420,'Upma':290,'Khichdi':420,'Biryani':900}
-    veg = ['pBM','palakPaneer','alooGobi','Bharta','Chole','Rajma','Bhindi','malaiKofta','Sambar','Dosa']
-    nonVeg = ['butterChicken','cTM','fishCurry','mRJ','Keema','tandooriChicken','Prawn','eggCurry','chickenBiryani','Korma']
-    snacks = ['Samosa','Pakora','bhelPuri','paniPuri','vadaPav','Dhokla','alooTikki','pavBhaji','Kachori','Chaat','Chips']
-    sweets = ['gulabJamun','Rasgulla','Jalebi','Laddu','Kheer','Barfi','Rasmalai','Halwa','Payasam','mysorePak']
-    print(veg)
-calories()
+from flask import Flask, request, jsonify,render_template
+app = Flask(__name__, template_folder='C:\VScode\Cryptics')
+Food = {'Roti': 120,'Naan': 270,'Paratha': 500,'Puri': 130,'Idli': 70,'Dosa':350,'Uttapam':420,'Upma':290,'Khichdi':420,'Biryani':900,
+        'pBM':300 ,'palakPaneer':220,'alooGobi':150,'Bharta':150,'Chole':250,'Rajma':270,'Bhindi':140,'malaiKofta':350,'Sambar':150,'Dosa':200,
+        'butterChicken':350,'cTM':300,'fishCurry':220,'mRJ':400,'Keema':300,'tandooriChicken':250,'Prawn':200,'eggCurry':180,'chickenBiryani':300,'Korma':350,
+        'Samosa':150,'Pakora':100,'bhelPuri':200,'paniPuri':50,'vadaPav':300,'Dhokla':80,'alooTikki':180,'pavBhaji':350,'Kachori':200,'Chaat':250,'Chips':170,
+        'gulabJamun':180,'Rasgulla':120,'Jalebi':150,'Laddu':200,'Kheer':250,'Barfi':180,'Rasmalai':220,'Halwa':250,'Payasam':230,'mysorePak':220}
+@app.route('/calculate_calories', methods=['POST'])
+def calculateCalories():
+    data = request.json
+    food = data.get('food')
+    count = data.get('count')
+    count=0 
+    if count in Food:
+      total_calories = Food[food] * count
+      return jsonify({'total_calories':total_calories})
+    else:
+       return jsonify({'Error':'Food item not found'}),400
+if __name__ == '__main__':
+    app.run(debug=True)

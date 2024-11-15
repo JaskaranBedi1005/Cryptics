953612077e0d941f1669a0becf36d92429345f0b
Hrideya <hrideyasharma7@gmail.com>
2024-11-15 22:16:04 +0530

    progrss

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 def calories():
-    stapleFood = ['Roti','Naan','Paratha','Puri','Idli','Dosa','Uttapam','Upma','Khichdi','Biryani']
-    veg = ['pBM','palakPaneer','alooGobi','Bharta','Chole','Rajma','Bhindi','malaiKofta','Sambar','Dosa']
+    stapleFood = {'Roti': 120,'Naan': 270,'Paratha': 500,'Puri': 130,'Idli': 70,'Dosa':350,'Uttapam':420,'Upma':290,'Khichdi':420,'Biryani':900}
+    veg = ['pBM','palakPaneeSr','alooGobi','Bharta','Chole','Rajma','Bhindi','malaiKofta','Sambar','Dosa']
     nonVeg = ['butterChicken','cTM','fishCurry','mRJ','Keema','tandooriChicken','Prawn','eggCurry','chickenBiryani','Korma']
     snacks = ['Samosa','Pakora','bhelPuri','paniPuri','vadaPav','Dhokla','alooTikki','pavBhaji','Kachori','Chaat','Chips']
     sweets = ['gulabJamun','Rasgulla','Jalebi','Laddu','Kheer','Barfi','Rasmalai','Halwa','Payasam','mysorePak']
-    categories = [stapleFood,veg,nonVeg,snacks,sweets]
-    print(categories[0][3])
 calories()

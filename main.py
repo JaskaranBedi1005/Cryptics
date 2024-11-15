7a610bec78cdbeab808bf593083b36e0978815ec
Hrideya <hrideyasharma7@gmail.com>
2024-11-16 03:45:50 +0530

    modified

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ Food = {'Roti': 120,'Naan': 270,'Paratha': 500,'Puri': 130,'Idli': 70,'Dosa':350
         'butterChicken':350,'cTM':300,'fishCurry':220,'mRJ':400,'Keema':300,'tandooriChicken':250,'Prawn':200,'eggCurry':180,'chickenBiryani':300,'Korma':350,
         'Samosa':150,'Pakora':100,'bhelPuri':200,'paniPuri':50,'vadaPav':300,'Dhokla':80,'alooTikki':180,'pavBhaji':350,'Kachori':200,'Chaat':250,'Chips':170,
         'gulabJamun':180,'Rasgulla':120,'Jalebi':150,'Laddu':200,'Kheer':250,'Barfi':180,'Rasmalai':220,'Halwa':250,'Payasam':230,'mysorePak':220}
+@app.route('/')
+def index():
+    return render_template('diets.html')
 @app.route('/calculate_calories', methods=['POST'])
 def calculateCalories():
     data = request.json
-    food = data.get('food')
-    count = data.get('count')
+    total_calories=0
     count=0 
-    if count in Food:
-      total_calories = Food[food] * count
-      return jsonify({'total_calories':total_calories})
-    else:
-       return jsonify({'Error':'Food item not found'}),400
+    for food_item,count in data.items():
+        if count in Food:
+           total_calories += Food[food] * count
+           return jsonify({'total_calories':total_calories})
+        else:
+             return jsonify({'Error':'Food item not found'}),400
 if __name__ == '__main__':
     app.run(debug=True)
